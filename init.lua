72e1f54481f9c8baf165b25cd9236413dee699cf
NguyenVux <nguyenhoangvub4@gmail.com>
2022-06-26 02:31:02 +0700

    update

--- init.lua
+++ init.lua
@@ -24,10 +24,33 @@ end
 
 function teleScope_config()
 	local telescope = require('telescope')
-	telescope.setup()
+	local action_layout = require("telescope.actions.layout")
+	telescope.setup({
+		defaults = { 
+		    mappings = { 
+		      i = {
+		        ["?"] = action_layout.toggle_preview,
+		      },
+		    },
+		  },
+		pickers = {
+			find_files = {
+			      theme = "dropdown",
+			}	
+		}
+	})
 
 	require('telescope').load_extension('fzf')
 	vim.api.nvim_set_keymap('','<C-p>',':Telescope find_files<CR>',{silent=true})
+end
+
+
+function lsp_config()
+	local lsp_config = require('lspconfig')
+	local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+	lsp_config.clangd.setup({
+    		capabilities = capabilities
+	})
 end
 
 function main()	
@@ -45,16 +68,29 @@ function main()
 		use 'shaunsingh/seoul256.nvim'
 		use 'nvim-lualine/lualine.nvim'
 		use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+
 		use {
         		'nvim-treesitter/nvim-treesitter',
 		        run = ':TSUpdate'
-    }
+		}
+		use {
+		    'numToStr/Comment.nvim'
+		}
+		use 'neovim/nvim-lspconfig'
+		use 'neovim/nvim-lspconfig'
+		use 'hrsh7th/cmp-nvim-lsp'
+		use 'hrsh7th/cmp-buffer'
+		use 'hrsh7th/cmp-path'
+		use 'hrsh7th/cmp-cmdline'
+		use 'hrsh7th/nvim-cmp'
 	end)
 	
 	NVim_config()
 	Setup_NVimTree()
 	Setup_Theme()
 	teleScope_config()
+	lsp_config()
+	require('Comment').setup()
 
 end
 
