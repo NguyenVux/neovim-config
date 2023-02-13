68ed37302d576c470b8a6e7177d72ca6e8883889
vux.nguyen <nguyenhoangvub4@gmail.com>
2023-02-13 21:07:09 +0700

    delete old setup

--- ginit.vim
+++
@@ -1,3 +0,0 @@
-GuiTabline 0
-silent! GuiFont! FiraCode Nerd Font Mono:h12
-call GuiWindowMaximized(1)
--- lua/tree_sitter_config/init.lua
+++
@@ -1,5 +0,0 @@
-require('nvim-treesitter.configs').setup {
-  ensure_installed = "all",
-  hightlight = { enable = true },
-  indent = { enable = true }
-}
