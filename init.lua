a6ad78ee1d157b1d7e18b4e149568c8f51d45fba
vux.nguyen <nguyenhoangvub4@gmail.com>
2023-02-14 00:31:12 +0700

    telescope

--- init.lua
+++ init.lua
@@ -38,6 +38,10 @@ packer.startup(function(use)
 		    ts_update()
 		end,
 	}
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.1',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
 	if packer_bootstrap then
 		require('packer').sync()
 	end
